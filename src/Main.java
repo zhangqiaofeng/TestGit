86453a617bfc3a71b996d7e9d2379f25f1dd92cd
zhangqiaofeng <quinnzander@163.com>
2018-05-16 17:57:12 +0800

    测试git

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
+        TestGit testGit = new TestGit();
+        System.out.println(testGit.hello()+"!");
         System.out.println("Hello World!");
     }
 }
